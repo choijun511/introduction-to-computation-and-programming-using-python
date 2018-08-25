0fcbe7f0dae2d28d49b2e4da9b1aecbe25a1da87
choijun511 <choijun@163.com>
2018-08-25 12:02:28 +0800

    再用字典实现一个斐波那契数列
    用字典实现一个斐波那契数列

--- practice006.py
+++ practice006.py
@@ -1,10 +1,22 @@
 # 用迭代的方式实现一个斐波那契数列
 
-def fib(x):
-    if x == 0 or x == 1:
-        return 1
-    else:
-        return fib(x-1) + fib(x-2)
+#def fib(x):
+#    if x == 0 or x == 1:
+#        return 1
+#    else:
+#        return fib(x-1) + fib(x-2)
 
-x = input('请输出一个自然数')
-print(fib(int(x)))
+#x = input('请输出一个自然数')
+#print(fib(int(x)))
+
+# 用字典实现一个斐波那契数列
+def fib_efficient(n, d):
+    if n in d:
+        return d[n]
+    else:
+        ans = fib_efficient(n-1, d) + fib_efficient(n-2, d)
+        d[n] = ans
+        return ans
+
+d = {1:1, 2:2}
+print(fib_efficient(6,d))
